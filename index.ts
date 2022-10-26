0d58cdc314b99471446c57c9bb8c140e20b9cc77
majorpeter <majorpeter29@gmail.com>
2022-10-26 16:45:14 +0200

    added force option

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import https from "https";
 import axios, {AxiosResponseHeaders} from "axios";
 import { readFileSync } from "fs";
+import { argv } from "process";
 
 import { getPfxByName as getPfxCertByName, installPfxCert, removePfxCertByName } from "./util";
 
@@ -11,7 +12,7 @@ const config: {
 
 const pfxName = 'tnscm';
 
-(async () => {
+async function renewCert(force: boolean): Promise<number> {
     const httpsAgent = new https.Agent({
         pfx: await getPfxCertByName(pfxName)
     });
@@ -22,10 +23,12 @@ const pfxName = 'tnscm';
         return -1;
     }
 
-    const remainingDays = parseInt(remainingReqResult.data);
-    if (remainingDays >= config.requiredRemainingDays) {
-        console.log(`Certificate still valid for ${remainingDays} days, no need to renew.`);
-        return 0;
+    if (!force) {
+        const remainingDays = parseInt(remainingReqResult.data);
+        if (remainingDays >= config.requiredRemainingDays) {
+            console.log(`Certificate still valid for ${remainingDays} days, no need to renew.`);
+            return 0;
+        }
     }
 
     const result = await axios.post(config.caUrl + '/renew', {}, {
@@ -39,6 +42,12 @@ const pfxName = 'tnscm';
             await removePfxCertByName(pfxName);
             await installPfxCert(result.data);
             console.log('Installed new cert!');
+            return 0;
         }
     }
-})();
+    return -2;
+}
+
+const args = argv.slice(2);
+const force = args.indexOf('--force') >= 0;
+renewCert(force);
